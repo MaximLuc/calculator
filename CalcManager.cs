d493ee2f7a7ff3fa309f08dffb1331e10c2c9fea
Maxim Pat <maximhop200307@gmail.com>
2022-11-22 18:55:44 +0200

    Prototype class CalcManager

--- CalcManager.cs
+++ CalcManager.cs
@@ -3,6 +3,31 @@ namespace Calculator
 {
     public class CalcManager
     {
-        
+        public double Add(double x, double y)
+        {
+            return x+y;
+        }
+        public double Sub(Double x, double y)
+        {
+            return x-y;
+
+        }
+        public double mul(Double x, double y)
+        {
+            return x*y;
+        }
+        public double del(Double x, double y)
+        {
+            if (y!=0)
+            {
+                return x/y;
+            }
+            else 
+            {
+                return 0;
+            }
+
+        }
+
     }
 }
